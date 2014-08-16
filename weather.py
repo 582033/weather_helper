1a33f74ca5ee2475ffbb8335936ef79b0496d889
Yan Jiang <j582033@163.com>
2014-08-16 16:17:23 +0800

    补全地区数据并增加数据源获取脚本

--- weather.py
+++ weather.py
@@ -6,19 +6,7 @@ import time
 import json
 import sys
 from threading import Thread
-
-city_code = {
-    'beijing': {'code':54511, 'name':u'北京'},
-    'chengdu': {'code':56294, 'name':u'成都'},
-    'hangzhou': {'code':58457, 'name':u'杭州'},
-    'changsha': {'code':57687, 'name':u'长沙'},
-    'wuhan': {'code':57494, 'name':u'武汉'},
-    'fuzhou':{'code':58847, 'name':u'福州'},
-    'ningbo':{'code':58465, 'name':u'宁波'},
-    'shaoyang':{'code':57766,'name':u'邵阳'},
-    'zibo':{'code':54830, 'name':u'淄博'},
-    'linzi':{'code':60815, 'name':u'临淄'}
-}
+from city_code import city_code
 
 def get_weather_by_city(city):
     base_url = "http://tianqi.2345.com"
